2c56848b5bcc6d251ab1b42dfa3236c865f1498a
dmathieu <42@dmathieu.com>
2010-09-29 15:07:27 +0200

    add callbacks to the user's support object

--- spec/support/user.rb
+++ spec/support/user.rb
@@ -1,5 +1,8 @@
 class User
+  extend ActiveModel::Callbacks
+  define_model_callbacks :create, :destroy
   include Rediline::User
+  
   rediline :timeline do
     list :egocentric do
       [user]
@@ -16,5 +19,16 @@ class User
   
   def self.find(id)
     self.new(id)
+  end
+  
+  def create
+    _run_create_callbacks do
+      # Your create action methods here
+    end
+  end
+  def destroy
+    _run_destroy_callbacks do
+      # Your destroy action methods here
+    end
   end
 end
